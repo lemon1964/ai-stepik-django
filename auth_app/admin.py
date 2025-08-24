55cb06b89b7bd8d9de8733472702559df7ba0f3d
lemon <lemon@example.com>
2025-08-24 20:28:53 +0300

    final no codes

--- auth_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'email', 'id', 'is_staff', 'is_superuser')
-    list_filter = ('is_staff', 'is_superuser')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
-
-admin.site.register(User, UserAdmin)
--- chat_app/types.py
+++
@@ -1,7 +0,0 @@
-# ai-chat-django/chat_app/types.py
-from enum import Enum
-
-class ModelType(str, Enum):
-    """Классификация поддерживаемых типов AI-моделей"""
-    TEXT = "text"    # Текстовые модели
-    CODE = "code"    # Модели генерации кода
